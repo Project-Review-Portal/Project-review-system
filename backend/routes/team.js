1efb19ad5c642d0bca6a56691ad4b8f5f19fb87c
Vibish <vibish246@gmail.com>
2026-06-15 15:45:17 +0530

    merge conflict resolved

--- backend/routes/team.js
+++ backend/routes/team.js
@@ -19,9 +19,7 @@ router.get('/my-assigned-panel', auth, teamController.getAssignedPanel);
 router.post('/request-guide', auth, teamController.requestGuide);
 router.delete('/my-team', auth, teamController.deleteMyTeam);
 
-<<<<<<< HEAD
 router.post('/report/upload', auth, reportUpload, teamController.uploadReport);
-=======
 router.get('/invitations', auth, teamController.getTeamInvitations);
 router.post('/respond-invitation', auth, teamController.respondToInvitation);
 router.post('/invite', auth, teamController.inviteMember);
@@ -30,8 +28,7 @@ router.post('/request-lock', auth, teamController.requestLock);
 router.post('/cancel-lock', auth, teamController.cancelLockRequest);
 router.post('/approve-lock', auth, teamController.approveLock);
 
-router.post('/report/upload', auth, upload, teamController.uploadReport);
->>>>>>> 2b9131a59ddb8af2864da03b92c55658b2c7897b
+// router.post('/report/upload', auth, upload, teamController.uploadReport);
 router.get('/report/status', auth, teamController.getReportStatus);
 
 // New route for fetching max team size for public (student) view
