fb108a30750b9bcc04a092d906b0a9ed11bbd573
Vibish <vibish246@gmail.com>
2026-08-07 13:36:41 +0530

    updateDb file updated to change other models proggrame from UG to B.E CSE

--- updateDB.cjs
+++ updateDB.cjs
@@ -1,13 +1,23 @@
 require('dotenv').config({ path: './backend/.env' });
 const mongoose = require('./backend/node_modules/mongoose');
 const User = require('./backend/models/User');
+const MaterialSettings=require('./backend/models/MaterialSetting');
+const Panel=require('./backend/models/Panel');
+const Team=require('./backend/models/Team');
+
 
 async function run() {
   const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017/project-review-system';
   console.log('Connecting to:', mongoUri);
   await mongoose.connect(mongoUri);
-  const result = await User.updateMany({ programme: "UG" }, { $set: { programme: "B.E. CSE" } });
-  console.log(result);
+  const result1 = await User.updateMany({ programme: "UG" }, { $set: { programme: "B.E. CSE" } });
+  const result2 = await MaterialSettings.updateMany({ programme: "UG" }, { $set: { programme: "B.E. CSE" } });
+  const result3 = await Panel.updateMany({ programme: "UG" }, { $set: { programme: "B.E. CSE" } });
+  const result4 = await Team.updateMany({ programme: "UG" }, { $set: { programme: "B.E. CSE" } });
+  console.log(result1);
+  console.log(result2);
+  console.log(result3);
+  console.log(result4);
   await mongoose.disconnect();
 }
 
